1de6b491dfc167c893d3dce67bf19058dd824262
Jonathan Poole <jonfpoole@gmail.com>
2021-09-21 18:32:41 +0100

    Migrate over the python tests (#7)
    * Migrate over the python tests
    
    * Add protoc
    
    * update tensorflow
    
    * setup-python
    
    * remove tensorflow

---
+++ test/data_dep_test.py
@@ -0,0 +1,18 @@
+"""Test on deps, data, requires and provides."""
+
+import os
+import subprocess
+import unittest
+
+
+class DataDepTest(unittest.TestCase):
+
+    def test_direct_dep(self):
+        """Test that we can import the module directly."""
+        from test import data_dep
+        self.assertEqual(42, data_dep.the_answer())
+
+    def test_data_dep(self):
+        """Test that we can also invoke the .pex directly as a data dependency."""
+        output = subprocess.check_output(['test/data_dep.pex'])
+        self.assertEqual('42', output.strip().decode('utf-8'))
---
+++ test/strip_source.py
@@ -0,0 +1,4 @@
+"""Test module for source stripping."""
+
+if __name__ == '__main__':
+    print('Test')
