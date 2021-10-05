e32a563fdd12effdbc128d31c230e51576fa546e
swestmoreland <swestmoreland@thoughtmachine.net>
2021-10-05 10:32:07 +0100

    Delete old e2e files

--- tools/wheel_resolver/src/e2e_test/e2e.py
+++
@@ -1,6 +0,0 @@
-from tools.wheel_resolver.src.wheel_tags.tags import get_download_urls, get_url
-
-
-def run(package:str, version:str=None, archs:list=[]):
-    download_urls = get_download_urls(package, version=None)
-    return get_url(download_urls, archs)
