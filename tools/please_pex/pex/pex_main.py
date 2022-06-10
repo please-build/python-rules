52dcd5ec6ccac4e6ce19c6c2d4fbdc4c7672ad69
Sam Westmoreland <samwestmoreland@users.noreply.github.com>
2022-06-10 15:50:03 +0100

    Migrate old python rules from please repo (#74)
    * Copy old rules across
    
    * Add testrunner_bootstrap config key
    
    * Update config variables
    
    * Copy over old pex tool
    
    * Rename new tool to v2
    
    * Remove unused function
    
    * Re-arrange third party deps to minimise changes
    
    * Update tool version
    
    * Get rid of preloads
    
    * Delete v2 rules
    
    * Make bools bools, otherwise flag checks in the build language don't work
    
    * Delete wheel tool and tests referencing deleted zip functions
    
    * Fix testrunner tests
    
    * Update changelog and trigger release from v1 branch
    
    * Update version and changelog in tools directory

--- tools/please_pex/pex/pex_main.py
+++ tools/please_pex/pex/pex_main.py
@@ -127,6 +127,8 @@ class SoImport(object):
             for name in zf.namelist():
                 path, _ = self.splitext(name)
                 if path:
+                    if path.startswith('.bootstrap/'):
+                        path = path[len('.bootstrap/'):]
                     importpath = path.replace('/', '.')
                     self.modules.setdefault(importpath, name)
                     if path.startswith(MODULE_DIR):
@@ -281,8 +283,7 @@ def explode_zip():
     inside a zipfile.
     """
     # Temporarily add bootstrap to sys path
-    ### THIS SHOULD MAYBE COME FROM CONFIG FILE?
-    sys.path = [os.path.join(sys.path[0], 'third_party/python')] + sys.path[1:]
+    sys.path = [os.path.join(sys.path[0], '.bootstrap')] + sys.path[1:]
     import contextlib, portalocker
     sys.path = sys.path[1:]
 
@@ -360,6 +361,8 @@ def main():
 
     N.B. This gets redefined by pex_test_main to run tests instead.
     """
+    # Add .bootstrap dir to path, after the initial pex entry
+    sys.path = sys.path[:1] + [os.path.join(sys.path[0], '.bootstrap')] + sys.path[1:]
     # Starts a debugging session, if defined, before running the entry point.
     start_debugger()
     # Must run this as __main__ so it executes its own __name__ == '__main__' block.
