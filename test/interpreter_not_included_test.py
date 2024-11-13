01f232865119da1397bd193bc731017d689c987a
Peter Ebden <peter.ebden@gmail.com>
2024-11-13 14:27:46 +0000

    Handle it

--- test/interpreter_not_included_test.py
+++ test/interpreter_not_included_test.py
@@ -8,7 +8,7 @@ class InterpreterNotIncludedTest(unittest.TestCase):
     def test_interpreter_is_not_included(self):
         """Test that we don't include the interpreter as a tool."""
         zf = zipfile.ZipFile(sys.argv[0])
-        names = [name for name in zf.namelist() if "third_party/cpython" in name]
+        names = [name for name in zf.namelist() if "third_party/cpython/lib" in name]
         self.assertFalse(names)
 
     def test_arcat_is_not_included(self):
