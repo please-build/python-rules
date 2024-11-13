bd2b13d35a4546818bb680f5bc6ec8548764ada1
Peter Ebden <peter.ebden@gmail.com>
2024-11-13 14:40:32 +0000

    Exclude in-repo python from pexes (#218)
    * add test
    
    * Handle it
    
    * version

---
+++ test/interpreter_not_included_test.py
@@ -0,0 +1,18 @@
+import sys
+import unittest
+import zipfile
+
+
+class InterpreterNotIncludedTest(unittest.TestCase):
+
+    def test_interpreter_is_not_included(self):
+        """Test that we don't include the interpreter as a tool."""
+        zf = zipfile.ZipFile(sys.argv[0])
+        names = [name for name in zf.namelist() if "third_party/cpython/lib" in name]
+        self.assertFalse(names)
+
+    def test_arcat_is_not_included(self):
+        """Test that arcat isn't included either."""
+        zf = zipfile.ZipFile(sys.argv[0])
+        names = [name for name in zf.namelist() if "arcat" in name]
+        self.assertFalse(names)
