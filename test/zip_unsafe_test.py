0d34acea3645f80f15ade71371b39015697b375a
Peter Ebden <peter.ebden@gmail.com>
2024-11-19 15:16:21 +0000

    fine we have to skip it entirely for now

--- test/zip_unsafe_test.py
+++ test/zip_unsafe_test.py
@@ -1,11 +1,9 @@
-import os
 import unittest
 
 
 class ZipUnsafeTest(unittest.TestCase):
     """Test importing something that requires zip-unsafety and exploding the pex."""
 
-    @unittest.skipIf(os.getenv("TOOL_DEV", "") != "true", "needs tool update to work")
     def test_can_import(self):
         from third_party.python.confluent_kafka import cimpl
         self.assertIsNotNone(cimpl)
