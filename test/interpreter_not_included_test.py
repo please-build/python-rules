6420f52da13c7de0dc142af4935e9e262ca576e4
Chris Novakovic <chris@chrisn.me.uk>
2025-12-01 02:32:33 +0000

    Fix interpreter path in in-repo interpreter test
    The in-repo interpreters were moved from `third_party/cpython/lib` to
    `third_party/cc/cpython` in #277, but the interpreter directory path in
    `//test:interpreter_not_included_test` wasn't also updated, so the test
    still passes even if please_pex erroneously packs the in-repo
    interpreter into the .pex file. Update the path to fix the test.

--- test/interpreter_not_included_test.py
+++ test/interpreter_not_included_test.py
@@ -6,9 +6,9 @@ import zipfile
 class InterpreterNotIncludedTest(unittest.TestCase):
 
     def test_interpreter_is_not_included(self):
-        """Test that we don't include the interpreter as a tool."""
+        """Test that we don't include any in-repo interpreter in the .pex file."""
         zf = zipfile.ZipFile(sys.argv[0])
-        names = [name for name in zf.namelist() if "third_party/cpython/lib" in name]
+        names = [name for name in zf.namelist() if "third_party/cc/cpython/" in name]
         self.assertFalse(names)
 
     def test_arcat_is_not_included(self):
